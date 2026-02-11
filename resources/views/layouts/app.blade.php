04f66c6bd58ae96ac0d7fdecb67e9555ebc27828
ronald2117 <abel.ronald2117@gmail.com>
2026-02-11 12:11:11 +0800

    feat(favicon): add favicon link to multiple pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'TSHSEMS - Evaluation Management')</title>
+    <link rel="icon" type="image/png" href="{{ asset('tshs_logo_whitebg.png') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-slate-50 font-sans">
--- resources/views/maintenance.blade.php
+++ resources/views/maintenance.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="{{ asset('tshs_logo_whitebg.png') }}">
     <title>System Maintenance - TSHSEMS</title>
     @vite(['resources/css/app.css'])
 </head>
